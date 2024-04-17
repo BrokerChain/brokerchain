09061a5d3e6b13544611a829a38f2bd3eb7826ff
Jianru Lin <jianru.lin@gmail.com>
2024-04-17 23:33:54 +0800

    node entry

---
+++ brokerchain-1.0.0/src/library/x-brokerchain-node/run/core.ts
@@ -0,0 +1,10 @@
+// initialized by dev/system
+
+import { Logger } from "../../../myutils/logger.js";
+import { Input, Output, Callback } from "./type.js";
+
+export async function core<R>(plog: Logger, input: Input, cb: Callback<R>): Promise<R> {
+    const log = plog.sub("x-brokerchain-node.run");
+    log.variable("input", input);
+    return cb.fail(log.new_error("TODO"));
+}
